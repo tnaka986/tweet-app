4712407b3dfa4b388557045c52df7e4b7b2d8067
tnaka986 <59523631+tnaka986@users.noreply.github.com>
2021-03-17 13:22:03 +0900

    Update DBManager.java

--- src/main/java/dao/DBManager.java
+++ src/main/java/dao/DBManager.java
@@ -11,12 +11,12 @@ public class DBManager {
 	public static Connection getCloudSQLConnection() {
 		
 		// Cloud SQL
-		String url = "jdbc:google:rdbms://lateral-apex-268306:us-central1:tweet-app/tweetapp";
+		String url = "jdbc:google:rdbms://<project-name>:<rejion>:tweet-app/tweetapp";
 		// ローカル用
 //		String url = "jdbc:mysql://localhost:3306/tweetapp";
 		
-		final String DB_USER = "mysql";
-		final String DB_PASS = "mysql";
+		final String DB_USER = "*****";
+		final String DB_PASS = "*****";
 		
 		try {
 			DriverManager.deregisterDriver(new AppEngineDriver());
